0fb14900b8c51fbc952295d9541a6a8bd3ca639b
Dicky Adryanzyah <adryanzyahdicky@gmail.com>
2020-12-23 00:45:22 +0700

    update

--- application/views/contents/home.php
+++ application/views/contents/home.php
@@ -79,7 +79,42 @@
         <div class="row">
             <div class="col-12">
                 <div class="section-title">
-                    <h2>Event Terbaru</h2>
+                    <h2>Product Terpopuler</h2>
+                </div>
+            </div>
+        </div>
+        <div class="top-movie-carousel owl-carousel">
+            <?php foreach ($event_terdekat as $key => $value) { ?>
+                <div class="single-top-movie">
+                    <div class="img">
+                        <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>">
+                            <img src="<?php echo $value->cover; ?>" alt="<?php echo str_replace('"',"`", $value->name); ?>" title="<?php echo str_replace('"',"`", $value->name); ?>">
+                        </a>
+                    </div>
+                    <span class="view-counter"><?php echo $value->code; ?></span>
+                    <div class="content">
+                        <h2 class="name">
+                            <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>"><?php echo $value->name; ?></a>
+                        </h2>
+                        <div class="price clearfix"><?php echo $value->category_name; ?></div>
+                        <p class="date"><?php echo (isset($value->price) && $value->price>0)?rupiah($value->price):'GRATIS'; ?></p>
+                        <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>"><p class="duration">Detail</p></a>
+                        <a href="<?php echo base_url(); ?>product/demo/<?php echo encrypt_decrypt('encrypt',$value->id); ?>" target="_blank" title="<?php echo str_replace('"',"`", $value->title); ?>"><p class="duration">Demo</p></a>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+<!-- top-movie-area-end -->
+
+<!-- top-movie-area-start -->
+<div class="upcoming-movie-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Product Terbaru</h2>
                 </div>
             </div>
         </div>
@@ -108,40 +143,7 @@
 </div>
 <!-- top-movie-area-end -->
 
-<!-- top-movie-area-start -->
-<div class="upcoming-movie-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="section-title">
-                    <h2>Event Terdekat</h2>
-                </div>
-            </div>
-        </div>
-        <div class="top-movie-carousel owl-carousel">
-            <?php foreach ($event_terdekat as $key => $value) { ?>
-                <div class="single-top-movie">
-                    <div class="img">
-                        <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>">
-                            <img src="<?php echo $value->cover; ?>" alt="<?php echo str_replace('"',"`", $value->name); ?>" title="<?php echo str_replace('"',"`", $value->name); ?>">
-                        </a>
-                    </div>
-                    <span class="view-counter"><?php echo $value->code; ?></span>
-                    <div class="content">
-                        <h2 class="name">
-                            <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>"><?php echo $value->name; ?></a>
-                        </h2>
-                        <div class="price clearfix"><?php echo $value->category_name; ?></div>
-                        <p class="date"><?php echo (isset($value->price) && $value->price>0)?rupiah($value->price):'GRATIS'; ?></p>
-                        <a href="<?php echo base_url(); ?>product/<?php echo $value->code; ?>" title="<?php echo str_replace('"',"`", $value->name); ?>"><p class="duration">Detail</p></a>
-                        <a href="<?php echo base_url(); ?>product/demo/<?php echo encrypt_decrypt('encrypt',$value->id); ?>" target="_blank" title="<?php echo str_replace('"',"`", $value->title); ?>"><p class="duration">Demo</p></a>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-</div>
-<!-- top-movie-area-end -->
+
 
 <script type="text/javascript">
     function link(link) {
