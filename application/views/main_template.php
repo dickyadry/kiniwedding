fe888cb5c17dfced1addfbe9a73e5d2286767ce3
Dicky Adryanzyah <adryanzyahdicky@gmail.com>
2020-12-31 11:04:27 +0700

    update main

--- application/views/main_template.php
+++ application/views/main_template.php
@@ -93,6 +93,87 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     <link href="<?php echo base_url('assets_member/'); ?>css/icons.css" rel="stylesheet" type="text/css">
     <link href="<?php echo base_url('assets_member/'); ?>css/app.css" rel="stylesheet" type="text/css">
 
+    <?php if(isset($detail)){ ?>
+      <?php if($type=="detail"){ ?>
+
+      <meta name="thumbnailUrl" content="<?php echo ASSETS . "img/logo.png";?>" itemprop="thumbnailUrl" />
+      <meta content="http//www.kiniwedding.com/" itemprop="url" />
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="#71C2EA">
+      <meta name="apple-mobile-web-app-title" content="kiniwedding">
+
+      <link rel="apple-touch-icon" href="<?php echo str_replace("medium", "small", $detail->cover); ?>" sizes="152x152">
+      <meta name="msapplication-TitleImage" content="<?php echo str_replace("medium", "small", $detail->cover); ?>">
+      <meta name="msapplication-TitleColor" content="#000">
+      <meta name="theme-color" content="#71C2EA">
+      <link rel="amphtml" href="<?php echo base_url(); ?>product/<?php echo $detail->code; ?>">
+      <meta name="adx:sections" content="read" /> 
+
+      <meta property="og:type" content="product" />
+      <meta property="fb:app_id" content="597752600925275" />
+      <meta property="og:site_name" content="kiniwedding" />
+      <meta name="ROBOTS" content="NOODP, NOYDIR" />
+      <meta property="og:title" content="Design Wedding Invitation By KiniWedding" />
+      <meta property="og:url" content="<?php echo base_url(); ?>product/<?php echo $detail->code; ?>" />
+      <meta property="og:image" content="<?php echo $detail->cover; ?>" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="400" />
+      <meta property="og:type" content="webminar" />
+      <meta property="og:description" content="Undangan Digital KiniWedding, desain responsive, trendi dan modern." />
+
+      <meta itemprop="image" content="<?php echo $detail->cover; ?>" />
+      <meta name="twitter:image:src" content="<?php echo $detail->cover; ?>" />
+      <meta itemprop="dateModified" content="<?php echo date('Y-m-d H:i:s'); ?>" />
+      <meta itemprop="datePublished" content="<?php echo date('Y-m-d H:i:s'); ?>" />
+      <meta itemprop="articleSection" content="Webinar" />
+
+      <meta name="twitter:site" content="@kiniwedding.com" />
+      <meta name="twitter:creator" content="@kiniwedding.com" />
+      <meta name="twitter:title" content="Design Wedding Invitation By KiniWedding" />
+      <meta name="twitter:description" content="Undangan Digital KiniWedding, desain responsive, trendi dan modern." />
+
+    <?php }}else{ ?>
+
+      <meta name="thumbnailUrl" content="<?php echo ASSETS . "img/logo.png";?>" itemprop="thumbnailUrl" />
+      <meta content="http//www.kiniwedding.com/" itemprop="url" />
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-status-bar-style" content="#71C2EA">
+      <meta name="apple-mobile-web-app-title" content="kiniwedding">
+
+      <link rel="apple-touch-icon" href="<?php echo str_replace("medium", "small", $detail->cover); ?>" sizes="152x152">
+      <meta name="msapplication-TitleImage" content="<?php echo str_replace("medium", "small", $detail->cover); ?>">
+      <meta name="msapplication-TitleColor" content="#000">
+      <meta name="theme-color" content="#71C2EA">
+      <link rel="amphtml" href="<?php echo base_url(); ?>product/<?php echo $detail->code; ?>">
+      <meta name="adx:sections" content="read" /> 
+
+      <meta property="og:type" content="product" />
+      <meta property="fb:app_id" content="387871355645473" />
+      <meta property="og:site_name" content="kiniwedding" />
+      <meta name="ROBOTS" content="NOODP, NOYDIR" />
+      <meta property="og:title" content="Design Wedding Invitation By KiniWedding" />
+      <meta property="og:url" content="https://kiniwedding.com" />
+      <meta property="og:image" content="<?php echo $detail->cover; ?>" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="400" />
+      <meta property="og:type" content="webminar" />
+      <meta property="og:description" content="Undangan Digital KiniWedding, desain responsive, trendi dan modern." />
+
+      <meta itemprop="image" content="<?php echo $detail->cover; ?>" />
+      <meta name="twitter:image:src" content="<?php echo $detail->cover; ?>" />
+      <meta itemprop="dateModified" content="<?php echo date('Y-m-d H:i:s'); ?>" />
+      <meta itemprop="datePublished" content="<?php echo date('Y-m-d H:i:s'); ?>" />
+      <meta itemprop="articleSection" content="Webinar" />
+
+      <meta name="twitter:site" content="@kiniwedding.com" />
+      <meta name="twitter:creator" content="@kiniwedding.com" />
+      <meta name="twitter:title" content="Design Wedding Invitation By KiniWedding" />
+      <meta name="twitter:description" content="Undangan Digital KiniWedding, desain responsive, trendi dan modern." />
+
+    <?php } ?>
+
     <style type="text/css">
     .full-loader{
       position: fixed; 
